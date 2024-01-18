680897b37bcd27cea25d5f3288085247bbcaea31
Bartek Pokrzywa <bartek.pokrzywa@gmail.com>
2024-01-18 23:57:57 +0100

    another fix

--- nvim_setup.sh
+++ nvim_setup.sh
@@ -3,7 +3,8 @@ HOME_DIR=/home/bpok
 wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz 
 tar -xzvf nvim-linux64.tar.gz
 rm -rf nvim-linux64.tar.gz
-cd $HOME_DIR && ln -s $HOME_DIR/nvim-linux64/bin/nvim /usr/local/bin/nvim
+rm -rf /usr/local/bin/nvim
+ln -s $HOME_DIR/nvim-linux64/bin/nvim /usr/local/bin/nvim
 mkdir -p $HOME_DIR/.config/nvim
 cp init.lua $HOME_DIR/.config/nvim/
 apt-get update && apt-get install unzip nodejs npm

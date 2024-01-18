8e7964e3ccf935966fc7459201fc9742af1659db
Bartek Pokrzywa <bartek.pokrzywa@gmail.com>
2024-01-18 23:54:17 +0100

    script reworked

--- nvim_setup.sh
+++ nvim_setup.sh
@@ -1,6 +1,9 @@
+HOME_DIR=/home/bpok
+
 wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz 
 tar -xzvf nvim-linux64.tar.gz
 rm -rf nvim-linux64.tar.gz
-cd /home/bpok && ln -s /home/bpok/nvim-linux64/bin/nvim /usr/local/bin/nvim
-mkdir -p /home/bpok/.config/nvim && cd /home/bpok/.config/nvim
-apt update && apt install unzip nodejs npm
+cd $HOME_DIR && ln -s $HOME_DIR/nvim-linux64/bin/nvim /usr/local/bin/nvim
+mkdir -p $HOME_DIR/.config/nvim
+cp init.lua $HOME_DIR/.config/nvim/
+apt-get update && apt-get install unzip nodejs npm

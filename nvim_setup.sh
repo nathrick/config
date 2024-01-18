2256ff3cd09f14fe9d28c3eefe3be1378c974bc3
Bartek Pokrzywa <bartek.pokrzywa@gmail.com>
2024-01-19 00:11:49 +0100

    path in symbolic link fixed

--- nvim_setup.sh
+++ nvim_setup.sh
@@ -4,7 +4,7 @@ wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar
 tar -xzvf nvim-linux64.tar.gz
 rm -rf nvim-linux64.tar.gz
 rm -rf /usr/local/bin/nvim
-ln -s $HOME_DIR/nvim-linux64/bin/nvim /usr/local/bin/nvim
+ln -s $HOME_DIR/config/nvim-linux64/bin/nvim /usr/local/bin/nvim
 mkdir -p $HOME_DIR/.config/nvim
 cp init.lua $HOME_DIR/.config/nvim/
 apt-get update

a09dc7edb087977119a1919593f110cd9f29d812
Bartek Pokrzywa <bartek.pokrzywa@gmail.com>
2024-01-20 01:10:48 +0100

    hope to be final version

---
+++ setup_root.sh
@@ -0,0 +1,8 @@
+USER=bpok
+HOME_DIR=/home/$USER
+
+sudo - $USER
+rm -rf /usr/local/bin/nvim
+ln -s $HOME_DIR/config/nvim-linux64/bin/nvim /usr/local/bin/nvim
+apt-get update
+apt-get -y install unzip nodejs npm python3-venv

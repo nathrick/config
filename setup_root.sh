6e25136d6097b5b13cbb986bb7cd224650580d2e
Bartek Pokrzywa <bartek.pokrzywa@gmail.com>
2024-01-22 10:02:57 +0100

    ripgrep added

--- setup_root.sh
+++ setup_root.sh
@@ -5,4 +5,4 @@ sudo - $USER
 rm -rf /usr/local/bin/nvim
 ln -s $HOME_DIR/config/nvim-linux64/bin/nvim /usr/local/bin/nvim
 apt-get update
-apt-get -y install unzip nodejs npm python3-venv
+apt-get -y install unzip nodejs npm python3-venv ripgrep

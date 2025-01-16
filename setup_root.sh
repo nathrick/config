3fa453b7a6b0d8c402618b17008b35baa1a666fc
bpok <bartek.pokrzywa@gmail.com>
2025-01-16 15:47:00 +0100

    Update setup_root.sh

--- setup_root.sh
+++ setup_root.sh
@@ -1,10 +1,10 @@
 USER=bpok
 HOME_DIR=/home/$USER
 
-sudo su - $USER
 rm -rf /usr/local/bin/nvim
 ln -s $HOME_DIR/config/nvim-linux64/bin/nvim /usr/local/bin/nvim
 apt-get update
 apt-get -y install unzip nodejs npm python3-venv ripgrep bear
 mkdir -p $HOME_DIR/.config/nvim
+chmod -R 777 $HOME_DIR/.config/nvim
 cp init.lua $HOME_DIR/.config/nvim/

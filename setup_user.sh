7fc60c569ac90ea13a292f60ad9ffa9c49deb548
bpok <bartek.pokrzywa@gmail.com>
2025-01-16 15:22:52 +0100

    Update setup_user.sh

--- setup_user.sh
+++ setup_user.sh
@@ -2,9 +2,6 @@ USER=bpok
 HOME_DIR=/home/$USER
 
 pip install "python-lsp-server[all]" python-lsp-isort pylsp-mypy python-lsp-black
-wget https://github.com/neovim/neovim/releases/download/v0.10.2/nvim-linux64.tar.gz
+wget https://github.com/neovim/neovim/releases/download/v0.10.3/nvim-linux64.tar.gz
 tar -xzvf nvim-linux64.tar.gz
 rm -rf nvim-linux64.tar.gz
-mkdir -p $HOME_DIR/.config/nvim
-cp init.lua $HOME_DIR/.config/nvim/
-

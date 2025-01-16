04eac6c503316f309d8fda585f90aafa01b22355
bpok <bartek.pokrzywa@gmail.com>
2025-01-16 15:24:13 +0100

    Update setup_root.sh

--- setup_root.sh
+++ setup_root.sh
@@ -6,3 +6,5 @@ rm -rf /usr/local/bin/nvim
 ln -s $HOME_DIR/config/nvim-linux64/bin/nvim /usr/local/bin/nvim
 apt-get update
 apt-get -y install unzip nodejs npm python3-venv ripgrep bear
+mkdir -p $HOME_DIR/.config/nvim
+cp init.lua $HOME_DIR/.config/nvim/
